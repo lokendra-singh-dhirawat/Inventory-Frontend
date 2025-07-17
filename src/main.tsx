7fa68edc5b3a798913cf4ede06640bd55c38a2ac
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-07-17 05:58:23 +0000

    setup main.tsx for react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import "./index.css";
 import { createHashRouter, RouterProvider } from "react-router";
 import ReactDom from "react-dom/client";
 import routes from "./routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 const router = createHashRouter(routes);
+
 ReactDom.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
