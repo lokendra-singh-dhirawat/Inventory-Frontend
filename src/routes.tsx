967b3633feef638abb74ae969abe3e436829d457
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-07-17 05:30:47 +0000

    format files and code

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import App from "./App";
+import NotFound from "./misc/NotFound";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFound />,
     Children: [
       {
         index: true,
