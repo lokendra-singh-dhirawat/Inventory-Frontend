8a71cddb062debc7e5f1035d212ded66df6580ea
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-07-18 07:58:37 +0000

    format code and fix some errors

--- src/misc/Header.tsx
+++ src/misc/Header.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 const Header = () => {
   return (
     <header className="flex items-center justify-between px-4 py-2 bg-primary shadow-sm border-b ">
-      <div className="md:hidden">
+      <div className="md:hid">
         <SidebarTrigger />
       </div>
       <h1 className="text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
